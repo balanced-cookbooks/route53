948c8bd5e0f1c8777b35def39cecbc622e912e98
Noah Kantrowitz <noah@coderanger.net>
2014-04-03 18:33:26 -0700

    Bump the version to make it clearer this is a fork.

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
 name             "route53"
-maintainer       "Heavy Water Software Inc."
-maintainer_email "darrin@heavywater.ca"
+maintainer       "Balanced, Inc."
+maintainer_email "dev@balancedpayments.com"
 license          "Apache 2.0"
 description      "Installs/Configures route53"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.4.0"
+version          "99.0.1" # Forked, force major to 99
